e90fb686d16c9b5ad5fc8018751e3ddea617e07c
Anohito-bit <glegoo6990@gmail.com>
2026-09-04 04:59:51 +0800

    Delete E-Commerce-Website-master directory

--- E-Commerce-Website-master/includes/functions.php
+++
@@ -1,7 +0,0 @@
-<?php
-function asset($relativePath)
-{
-    $fullPath = __DIR__ . '/../' . $relativePath;
-    $version = file_exists($fullPath) ? filemtime($fullPath) : time();
-    return $relativePath . '?v=' . $version;
-}
